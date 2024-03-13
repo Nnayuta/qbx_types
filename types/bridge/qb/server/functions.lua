f6121766b108604ebfcc051d814aa733e96a4264
Nayuta <naayudan@gmail.com>
2024-03-12 21:14:15 -0300

    .,

--- types/bridge/qb/server/functions.lua
+++ types/bridge/qb/server/functions.lua
@@ -8,13 +8,3 @@ local QBCore = {}
 ---comment
 ---@return qb-core
 function exports['qb-core']:GetCoreObject() end
-
-
---- —[ bridge qb server: functions ]—
--- Add or change (a) field(s) in the QBCore table
----@deprecated
----@param fieldName string
----@param data any
----@return boolean success
----@return string message
-function QBCore:SetField(fieldName, data) end
